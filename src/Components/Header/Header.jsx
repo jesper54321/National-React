c2be2a27c496d7a06e7f61cd5b0699a26b1cd733
mikk7021 <mikk7021@gmail.com>
2022-03-16 12:54:21 +0100

    added stuff on home

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser } from '@fortawesome/free-solid-svg-icons'
 import style from './header.module.scss'
-import Home from '../Home/Home'
+import { NavLink } from 'react-router-dom'
 
 
 export default function Header () {
@@ -11,7 +11,7 @@ export default function Header () {
             <FontAwesomeIcon className={style.iconcolor} icon={faUser} />
             <h4>ALiteralNameOh</h4>
             </div>
-            <button onClick={Home}>Exit</button>
+            <NavLink to="/">Exit</NavLink>
         </header>
     )
 }
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import Footer from "../Footer/Footer";
-import Header from "../Header/Header";
+import style from './home.module.scss'
+import houseimg from '../assets/images/Isometric-House.svg'
+import { NavLink } from "react-router-dom";
+
 
 
 export default function Home() {
-	return <div>
-		<Header/>
-		<Footer/>
+	return <div className={style.homeclass}>
+		<img src={houseimg} alt="image_of_house" />
+		<h2>Feel at home!</h2>
+		<NavLink>Login</NavLink>
+		<NavLink>Register</NavLink>
 	</div>;
 }
