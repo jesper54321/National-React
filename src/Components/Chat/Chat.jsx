6ef9d2b4c2eef786d0a0ee85b6861e28c75a2957
Hydrille <03rytter@gmail.com>
2022-03-16 08:58:34 +0000

    add messages

--- src/Components/Chat/Chat.jsx
+++ src/Components/Chat/Chat.jsx
@@ -1,5 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Message from "./Message/Message";
+import styles from "./Chat.module.scss";
+import {
+	getFirestore,
+	collection,
+	onSnapshot,
+	query,
+} from "firebase/firestore";
+import { app } from "../../Logic/firebase.js";
+
+const db = getFirestore(app);
+
+const incomingColor = "";
+const outgoingColor = "";
 
 export default function Chat() {
-	return <div>Chat</div>;
+	const [Chats, setChats] = useState([]);
+	const [Users, setUsers] = useState([]);
+	const [UserRef, setUserRef] = useState([]);
+
+	useEffect(async () => {
+		const q = await query(collection(db, "Chats"));
+		const unsubscribe = await onSnapshot(q, (querySnapshot) => {
+			const chatArray = [];
+			querySnapshot.forEach((doc) => {
+				chatArray.push(doc.data());
+			});
+			setChats([...chatArray]);
+		});
+	}, []);
+
+	console.log(Chats);
+
+	return (
+		<>
+			<div>
+				<ul className={styles.messageList}>
+					{Chats?.map((item, index) => {
+						return (
+							<Message
+								data={item}
+								type="outgoing"
+								key={index}
+								styles={styles}
+							/>
+						);
+					})}
+				</ul>
+			</div>
+		</>
+	);
 }
