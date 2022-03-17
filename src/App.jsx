cb188d989843913c6d499f62f4bfc6957dbe7da4
Selia2000 <tidjanimounira@alumno.elrincon.es>
2022-03-17 10:49:58 +0000

    before pull from development

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import { Login } from './component/Login';
+//import './App.css';
+//import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+//import { Login } from './Components/Login/Login';
 import "./App.scss";
-//import Routes from "./Wrappers/Routing";
-//import PageControl from "./Wrappers/PageControl";
-//import AuthProvider from "./Wrappers/AuthProvider";
+import Routes from "./Wrappers/Routing";
+import PageControl from "./Wrappers/PageControl";
+import AuthProvider from "./Wrappers/AuthProvider";
 
 
-
-
-function App() {
-  return (
-    <div className="App">
-
-      <Router>
-        
-        <Routes>
-            <Route path='/Login' element = {<Login/>}></Route>
-        </Routes>
-        
-  
-      </Router>
-      
-    </div>
-    );
-  }
 
 
 // function App() {
-// 	return (
-// 		<AuthProvider>
-// 			<PageControl>
-// 				<Routes />
-// 			</PageControl>
-// 		</AuthProvider>
-// 	);
-// }
+//   return (
+//     <div className="App">
+
+//       <Router>
+        
+//         <Routes>
+//             <Route path='/Login' element = {<Login/>}></Route>
+//         </Routes>
+        
+  
+//       </Router>
+      
+//     </div>
+//     );
+//   }
+
+
+function App() {
+	return (
+		<AuthProvider>
+			<PageControl>
+				<Routes />
+			</PageControl>
+		</AuthProvider>
+	);
+}
 
 export default App;
--- src/Wrappers/AuthProvider.jsx
+++ src/Wrappers/AuthProvider.jsx
@@ -20,5 +20,7 @@ export default function AuthProvider(props) {
 		// An error happened.
 	});
 
+	
+
 	return <>{props.children}</>;
 }
