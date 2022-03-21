274da82c24d9707e345e08301e2538083e353ef3
Asoret <asoretoverpower@gmail.com>
2022-03-21 09:08:19 +0000

    Finished register, for real this time?

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,17 +2,24 @@ import React, { useEffect, useState } from "react";
 import "./Register.scss";
 //import { auth } from "../../Wrappers/AuthProvider";
 import { useNavigate } from "react-router-dom";
-import FirebaseMain,{ db }  from "../../Logic/firebase";
-import AuthProvider, { auth } from "../../Wrappers/AuthProvider";
+import FirebaseMain, { db } from "../../Logic/firebase";
+import { auth } from "../../Wrappers/AuthProvider";
 import { SetUser } from "../../Wrappers/AuthProvider";
 import { getFirestore, collection, onSnapshot, query, addDoc, serverTimestamp } from "firebase/firestore";
 import { createUserWithEmailAndPassword } from "firebase/auth";
+import CustomPopup from "./CustomPopup";
 
 export default function Register() {
 	const [username, setUsername] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const [photo, setPhoto] = useState("");
 
+	const [visibility, setVisibility] = useState(false);
+
+	const popupCloseHandler = (e) => {
+		setVisibility(e);
+	};
 
 	const navigate = useNavigate();
 
@@ -43,6 +50,7 @@ export default function Register() {
 			username: username,
 			email: email,
 			date: serverTimestamp(),
+			photo: photo,
 		});
 	};
 
@@ -58,7 +66,7 @@ export default function Register() {
 	}
 
 	function checkErrors() {
-		
+
 		if (username !== "") {
 			if (usernames.includes(username)) {
 				setError(document.getElementById("usernameIn"), "Username is taken");
@@ -90,6 +98,12 @@ export default function Register() {
 			setSuccess(document.getElementById("passwordIn"))
 			rightPassword = true;
 		}
+
+		if(photo !== ""){
+			setSuccess(document.getElementById("photoIn"));
+		}else{
+			setError(document.getElementById("photoIn"), "Choose a photo");
+		}
 	}
 
 
@@ -100,7 +114,7 @@ export default function Register() {
 			<form onSubmit={(event) => {
 				event.preventDefault();
 				checkErrors();
-				if (rightEmail && rightPassword && rightUser) {
+				if (rightEmail && rightPassword && rightUser && (photo !== "")) {
 					createUserWithEmailAndPassword(auth, email, password)
 						.then((userCredential) => {
 							// Signed in 
@@ -127,7 +141,7 @@ export default function Register() {
 						id="usernameIn"
 						type="username"
 						value={username}
-						onChange={(event) => setUsername(event.target.value) + checkErrors() }
+						onChange={(event) => setUsername(event.target.value) + checkErrors()}
 						onClick={(event) => checkErrors()}
 					/>
 					<div className="error"></div>
@@ -152,17 +166,38 @@ export default function Register() {
 						onChange={(event) => setPassword(event.target.value) + checkErrors()}
 						onClick={(event) => checkErrors()}
 					/>
-					<div className="error"></div>
+					<div className="error"></div><br></br>
 				</div>
 				<div className="input-control">
+					<label>Profile photo</label>
+					<img src="https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/image-icon-png-6.jpg?alt=media&token=6d98349b-cb4f-424e-807c-ae1956d07ad3"
+						width="40" height="50" onClick={(e) => setVisibility(!visibility)} />
 					<input
-						id="photodIn"
-						type="username"
-						value={password}
-						onChange={(event) => setPassword(event.target.value)}
-						style={{backgroundColor:"#f0f0f098"}}
+						id="photoIn"
+						type="photo"
+						value={photo}
+						style={{ backgroundColor: "#f0f0f098", width: '70%', display: 'inline-block' }}
 						readOnly
 					/>
+					
+					<CustomPopup
+						onClose={popupCloseHandler}
+						show={visibility}
+						title="Choose a profile photo:"
+					>
+						<img src="https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile1.PNG?alt=media&token=e01d55ea-c50a-4720-96be-7b7dedf4af8e"
+						width="150" height="170" onClick={(e) => setVisibility(!visibility) + 
+							setPhoto("https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile1.PNG?alt=media&token=e01d55ea-c50a-4720-96be-7b7dedf4af8e")
+							+ checkErrors() + setSuccess(document.getElementById("photoIn"))}/>
+						<img src="https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile2.PNG?alt=media&token=c96ee1b7-7cc0-415c-9c42-4dc19a7d32db"
+						width="150" height="170" onClick={(e) => setVisibility(!visibility) + 
+							setPhoto("https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile2.PNG?alt=media&token=c96ee1b7-7cc0-415c-9c42-4dc19a7d32db")
+							+ checkErrors() + setSuccess(document.getElementById("photoIn"))}/>
+						<img src="https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile3.PNG?alt=media&token=da64a5a6-3edb-4824-9c6a-4f3ac48c1c22"
+						width="150" height="170" onClick={(e) =>  setVisibility(!visibility) +
+						setPhoto("https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/profile3.PNG?alt=media&token=da64a5a6-3edb-4824-9c6a-4f3ac48c1c22")
+						+ checkErrors() + setSuccess(document.getElementById("photoIn"))}/>
+					</CustomPopup>
 
 					<div className="error"></div>
 				</div>
@@ -170,6 +205,7 @@ export default function Register() {
 				<br></br><br></br>
 				<h2>Already have an account?</h2>
 				<button>Log in</button>
+
 			</form>
 		</div>
 	);
