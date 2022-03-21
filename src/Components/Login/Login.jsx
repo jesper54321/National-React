7ff40e3d86bd6a1db97880a65b96e86edaf55081
Hydrille <03rytter@gmail.com>
2022-03-21 11:55:14 +0000

    update selia's changes

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,102 +1,129 @@
-import React, {useState, useEffect} from "react";
-import './Login.scss';
-//import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useState, useEffect } from "react";
+import styles from "./Login.module.scss";
+import { useHistory, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { db } from "../../Logic/firebase";
+import { auth, username } from "../../Wrappers/AuthProvider";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import FirebaseMain from "../../Logic/firebase";
+import { SetUser } from "../../Wrappers/AuthProvider";
 
 export default function Login() {
-	return <div id="login">Login</div>;
+	var emailLogin;
+	var usernameLogin;
+
+	const initialValues = { email: "", password: "", username: "" };
+	const [formValues, setFormValues] = useState(initialValues);
+	const [formErrors, setFormErrors] = useState({});
+	const [isSubmit, setIsSubmit] = useState(false);
+	const [user, loading, error] = useAuthState(auth);
+
+	const navigate = useNavigate();
+
+	const handleChange = (e) => {
+		console.log(e.target);
+		const { name, value } = e.target;
+		setFormValues({ ...formValues, [name]: value });
+
+		console.log(formValues);
+	};
+
+	var usersData = FirebaseMain();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		setFormErrors(validate(formValues));
+		setIsSubmit(true);
+
+		for (var i = 0; i < usersData.Users.length; i++) {
+			if (usersData.Users[i].email === formValues.email) {
+				emailLogin = formValues.email;
+				usernameLogin = usersData.Users[i].username;
+
+				//SetUser(usersData.Users[i].username, usersData.Users[i].email) ;
+			}
+			if (usersData.Users[i].username === formValues.email) {
+				//SetUser(usersData.Users[i].username, usersData.Users[i].email);
+				formValues.email = usersData.Users[i].email;
+
+				emailLogin = usersData.Users[i].email;
+				usernameLogin = usersData.Users[i].username;
+			}
+		}
+
+		signInWithEmailAndPassword(auth, formValues.email, formValues.password)
+			.then((userCredential) => {
+				// Signed in
+				const user = userCredential.user;
+
+				console.log("login successfuly");
+
+				SetUser(usernameLogin, emailLogin);
+
+				navigate("/");
+			})
+			.catch((error) => {
+				document.getElementById("wrong").innerHTML = "Wrong data!";
+				const errorCode = error.code;
+				const errorMessage = error.message;
+			});
+	};
+
+	const validate = (values) => {
+		const errors = {};
+		//const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
+
+		if (!values.email) {
+			errors.email = "Email or username ares required!";
+		}
+		if (!values.password) {
+			errors.password = "Password is required!";
+		} else if (values.password.length < 6) {
+			errors.password = "Password must be more than 6 caracters";
+		}
+
+		return errors;
+	};
+	return (
+		<div className={styles.loginWrapper}>
+			<form onSubmit={handleSubmit} className={styles.loginForm}>
+				<h2>LOG IN </h2>
+
+				<div>
+					<p>{formErrors.email}</p>
+					<input
+						type="text"
+						placeholder="enter your email or your username, please"
+						name="email"
+						value={formValues.email}
+						onChange={handleChange}
+					/>
+				</div>
+
+				<div>
+					<p>{formErrors.password}</p>
+					<input
+						type="password"
+						placeholder="Enter your password"
+						name="password"
+						value={formValues.password}
+						onChange={handleChange}
+					/>
+				</div>
+
+				<div>
+					<button>Submit</button>
+				</div>
+
+				<div>
+					<h4>Don't have account yet ?</h4>
+					<Link to="../Register">
+						<button>Register</button>
+					</Link>
+				</div>
+			</form>
+			<div id="wrong"></div>
+		</div>
+	);
 }
-
-// 	export const login = () =>{
-//    		const initialValues = {username:"", password:""};
-// 		const [formValues, setFormValues] = useState(initialValues);
-//     	const [formErrors, setFormErrors] = useState({});
-//     	const [isSubmit, setIsSubmit] = useState(false);
-
-// const handleChange = (e) => {
-//    console.log (e.target);
-//    const { name, value } = e.target;
-//     setFormValues({...formValues, [name]: value});
-
-//     console.log(formValues);
-// };
-
-//   const handleSubmit = (e) => {
-//     e.preventDefault();
-//     setFormErrors(validate(formValues));
-//     setIsSubmit(true);
-
-  
-//     // const auth = getAuth();
-//     //  signInWithEmailAndPassword(auth, e.email, e.password)
-//     //     .then((userCredential) => {
-//     //     // Signed in 
-//     //   const user = userCredential.user;
-//     //  // ...
-//     // })
-//     // .catch((error) => {
-//     // const errorCode = error.code;
-//     // const errorMessage = error.message;
-//     // });
-    
-// };
-
-// useEffect (() => {
-//     console.log(formErrors);
-//     if(Object.keys(formErrors).length === 0 && isSubmit){
-//         console.log(formValues);
-//     }
-// },[formErrors]);
-
-// const validate = (values)=>{
-//     const errors = {};
-//    //const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/ ;
-
-//     if(!values.username){
-//         errors.username = "Username is required!";
-//     }
-//     if(!values.password){
-//         errors.password = "Password is required!";
-//     }else if (values.password.length < 8)
-//     {
-//         errors.password = "Password must be more than 8 caracters";
-//     }
-
-//     return errors;
-// };
-//     return(
-
-//         <div>
-//             {Object.keys(formErrors).length === 0 && isSubmit ? (<div className="messageSucess">Signed in successfully</div>):
-//             (<pre>{JSON.stringify (formValues, undefined, 2)}</pre>)}
-
-            
-//             <form  onSubmit={handleSubmit}> 
-//             <h2 >LOG IN </h2>
-            
-//             <div>
-//                 <p>{ formErrors.username}</p>
-//                 <input type="text" placeholder="enter your username, please"  name ="username" value={formValues.username} onChange={handleChange}/>
-//             </div>
-            
-//             <div>
-//             <p>{ formErrors.password}</p>
-//                 <input type="password" placeholder="Enter your password" name ="password"  value={formValues.password} onChange={handleChange} />
-//             </div>
-            
-//             <div>
-//                     <button>Submit</button>
-//             </div>
-
-            
-//             <div>
-//                 <h4>Don't have account yet ?</h4>
-//                 <button>Register</button>
-//             </div>
-
-//             </form>
-            
-             
-//         </div>
-    
-//     );
-// }
