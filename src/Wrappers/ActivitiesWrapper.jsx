1f0220c2b80440253d6c0ecbb2caa8a20ebb07b6
Hydrille <03rytter@gmail.com>
2022-03-22 13:52:44 +0000

    updating styles adding details

--- src/Wrappers/ActivitiesWrapper.jsx
+++ src/Wrappers/ActivitiesWrapper.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Outlet } from "react-router-dom";
-import Header  from "../Components/Header/Header"
-import Footer from "../Components/Footer/Footer"
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
+import { pullCollection } from "../Logic/firebase";
 
 export default function ActivitiesWrapper() {
+	useEffect(async () => {
+		const Places = await pullCollection("Places");
+		sessionStorage.setItem("PLACES", JSON.stringify(Places));
+	}, []);
+
 	return (
 		<>
-			<Header/>
-			<Outlet />
-			<Footer/>
+			<Header />
+			<section className="activityWrapper">
+				<Outlet />
+			</section>
+			<Footer />
 		</>
 	);
 }
