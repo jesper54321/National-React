dda0cbaf422c44db746e28f6e635c96cb826646f
Selia2000 <tidjanimounira@alumno.elrincon.es>
2022-03-23 13:51:59 +0000

    working on the list

--- src/Components/Activities/List/List.jsx
+++ src/Components/Activities/List/List.jsx
@@ -1,46 +1,83 @@
 import { faRoad } from "@fortawesome/free-solid-svg-icons";
-import React, {useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
+// import { useHistory } from "react-router-dom";
 import styles from "./List.module.scss";
-import {pullCollection} from "../../../Logic/firebase";
+import { pullCollection } from "../../../Logic/firebase";
 import { async } from "@firebase/util";
 
 
-export default function List() {
-	
-	const [places, setPlaces ] = useState ([]);
+const myPlaces = [
+	{
+		id: 1,
+		name: "Fat Frank",
+		sustainableId: 1
+	}, {
+		id: 2,
+		name: "Fat Frank",
+		sustainableId: 1
+	}, {
+		id: 3,
+		name: "Fat Frank",
+		sustainableId: 1
+	}, {
+		id: 4,
+		name: "Fat Frank",
+		sustainableId: 1
+	}
+];
+
+export default function List(props) {
+	// const history = useHistory();
+
+	const [places, setPlaces] = useState([]);
 	const [images, setimages] = useState([]);
 
-	useEffect( async () => {
+	useEffect(async () => {
 		setPlaces(await pullCollection("Places"));
 
-	}, [])
-		
-	return (
-		<div className={styles.listBackground}>
-			<div>
-				<div className={styles.listImage}>
-					<div ></div>
-				</div>
-				<div className={styles.listImageName}>
-						<h1>Moutain of somewhere</h1>
-				</div>
-			</div>
+	}, []);
 
-			<div>
-				<div className={styles.listImage}>
-					<div ></div>
-				</div>
-				<div className={styles.listImageName}>
-						<h1>Moutain of somewhere</h1>
-				</div>
-			</div>	
-		</div>
+	function placeDetails(id) {
+		props.history.push(`activities/details/${id}`);
+	}
+
+	return (
+		<>
+			<div className={styles.listBackground} url="/asset">
+
+				{
+					myPlaces.map((p) => {
+
+						console.log(p);
+
+						return (
+
+							<div key={p.id} onClick={(p) => placeDetails(p.id)}>
+								<div className={styles.listImage}
+									style={{ backgroundImage: `url("./places/place-${p.id}.jpg")` }}>
+									<img src={`./objectives/objective-${p.sustainableId}.png`} className={styles.objectiveImage} />
+									{/* <div className={styles.objectiveImage}
+									style={{ backgroundImage: `url("./objectives/objective-${p.sustainableId}.png")` }}></div> */}
+								</div>
+								<div className={styles.listImageName}>
+									<h1>Moutain of somewhere</h1>
+								</div>
+							</div>
+						)
+					}
+
+					)
+				}
+
+
+			</div>
+		</>
 	);
 }
 
 
-				
-				{/* <div className={styles.listPlaceName}>
+
+{/* <div className={styles.listPlaceName}>
 					{places.map((items)=>{
 						return (<div> 
 							{items.name}
@@ -48,4 +85,4 @@ export default function List() {
 				})}	
 				</div> */}
 
-		
+
