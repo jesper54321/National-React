62674887f71738adbcdb950f5976d3e7642fc295
Asoret <asoretoverpower@gmail.com>
2022-03-23 12:09:35 +0000

    Map home button

--- src/Components/Activities/Map/Map.jsx
+++ src/Components/Activities/Map/Map.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { MapContainer, TileLayer, Marker, Popup, useMapEvent } from "react-leaflet";
 import tileLayer from "../../../util/tileLayer";
+import styles from "./Map.module.scss";
 import 'leaflet/dist/leaflet.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
@@ -37,6 +37,65 @@ var entry = 0;
 
 const center = [28.140705, -15.428731];
 
+const HomeButton = ({ map }) => {
+	useMapEvent({
+	  dragend() {
+		const { lat: latD, lng: lngD } = map.getCenter();
+		const { lat, lng } = map.getCenter();
+  
+		const checkEqualArrays =
+		  [lat, lng] !== [latD.toFixed(5) * 1, lngD.toFixed(5) * 1];
+  
+		document.body.classList[checkEqualArrays ? "add" : "remove"](
+		  "show-button-home"
+		);
+	  },
+	});
+  
+	useEffect(() => {
+	  if (!map) return;
+  
+	  const customControler = L.Control.extend({
+		options: {
+		  position: "topleft",
+		},
+  
+		onAdd: function () {
+		  const btn = L.DomUtil.create("button", "back-to-home");
+		  btn.title = "pop rotation";
+
+		  btn.innerHTML =
+			'<img src=\'https://firebasestorage.googleapis.com/v0/b/national-react-app.appspot.com/o/home-start-top-icon-image-flat-home-icon-logo-symbol-sphere-building-transparent-png-400306.png?alt=media&token=3b4f2141-accc-49e9-b4e2-31b088278151\'width=\'25px\' height=\'25px\'>';
+  
+		  btn.onclick = function () {
+			map.flyToBounds([center]);
+			document.body.classList.remove("show-button-home");
+		  };
+  
+		  return btn;
+		},
+	  });
+  
+	  map.addControl(new customControler());
+  
+	  const info = L.Control.extend({
+		options: {
+		  position: "bottomleft",
+		},
+  
+		onAdd: function () {
+		  const info = L.DomUtil.create("div", "legend");
+		  info.textContent = "move the map";
+		  return info;
+		},
+	  });
+  
+	  map.addControl(new info());
+	}, [map]);
+  
+	return null;
+  };
+
 const content1 = [
 	{
 		title: "Sukiennice",
@@ -44,11 +103,22 @@ const content1 = [
 			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg/1920px-A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg",
 		style: { width: "200px" },
 		text: "Place 1",
-		link: "https://en.wikipedia.org/wiki/Krak%C3%B3w",
+	},
+];
+
+const content2 = [
+	{
+		title: "Sukiennice",
+		image:
+			"https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg/1920px-A-10_Sukiennice_w_Krakowie_Krak%C3%B3w%2C_Rynek_G%C5%82%C3%B3wny_MM.jpg",
+		style: { width: "200px" },
+		text: "Place 2",
 	},
 ];
 
 const MapWrapper = () => {
+	const [map, setMap] = useState(null);
+
 	React.useEffect(() => {
 		const L = require("leaflet");
 
@@ -70,10 +140,11 @@ const MapWrapper = () => {
 	}
 
 	return (
-		<div className="grid" style={{ width: "100%", height: "100%", margin: "0", padding: "0" }}>
+		<div className={styles[MapContainer]} style={{ width: "100%", height: "100%", margin: "0", padding: "0"}}>
+		<div  className={styles["grid"]} style={{ width: "100%", height: "100%", margin: "0", padding: "0" }}>
 			<ToastContainer limit={1} />
 			<main style={{ position: "absolute", bottom: "0%" }}>
-				<MapContainer center={center} zoom={17.2} scrollWheelZoom={false} draggable={true}
+				<MapContainer center={center} zoom={17.2} scrollWheelZoom={false} draggable={true} whenCreated={setMap}
 					style={{ width: "100%", height: "100%", margin: "auto", borderStyle: "solid", borderWidth: "2px" }}>
 					<TileLayer {...tileLayer} />
 					<Marker position={center}>
@@ -92,9 +163,26 @@ const MapWrapper = () => {
 
 						</Popup>
 					</Marker>
+					<Marker position={[28.14200, -15.428731]}>
+						<Popup maxWidth={200}>
+							{content2.map((item, index) => (
+								<div key={index}>
+									<figure>
+										<img src={item.image} alt={item.title} style={item.style} />
+									</figure>
+									<div>
+										{item.text}<br></br>
+										<NavLink to="/activities/details/1">Show more</NavLink>
+									</div>
+								</div>
+							))}
+
+						</Popup>
+					</Marker>
+					<HomeButton map={map} />
 				</MapContainer>
 			</main>
-		</div>
+		</div></div>
 	);
 };
 
